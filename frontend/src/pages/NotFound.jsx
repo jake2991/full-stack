bb82c98d077ccb25c59e020a0b659414fc0dc906
jake2991 <jakemy2991@gmail.com>
2025-03-04 09:25:00 -0700

    initialize backend and frontend structure with basic configurations and components

---
+++ frontend/src/pages/NotFound.jsx
@@ -0,0 +1,13 @@
+
+function NotFound(){
+    return (
+        <>
+        <div>
+            <h1>404 not Found</h1>
+            <p>the page youre looking for does not exist</p>
+        </div>
+        </>
+    )
+}
+
+export default NotFound
